7bf3f803bac67db26f9511beed946539ccd22614
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2015-11-14 18:40:39 -0500

    by passing unexpected error during post to server

--- monitoring_node/main.py
+++ monitoring_node/main.py
@@ -37,7 +37,10 @@ if __name__ == '__main__':
             print "\n"
 
 
-	    
+
             time.sleep(60)
         except KeyboardInterrupt:
             exit()
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            time.sleep(60)
