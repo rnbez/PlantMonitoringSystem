775cb299bb701dff4d0d7f4d3afcd6f3931b85ac
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2015-11-26 15:15:42 -0500

    including current state information in the log

--- monitoring_node/log.py
+++ monitoring_node/log.py
@@ -4,14 +4,16 @@ current_state = ''
 log_file = 'sys.log'
 def log_error(msg):
     global log_file
+    global current_state
     f = open(log_file, 'a')
     date = datetime.datetime.now().isoformat()
-    f.write("Error @ {}\n*******\n{}\n\n".format(date, msg))
+    f.write("Error @ {}\nState: {}\n*******\n{}\n\n".format(date, current_state, msg))
     f.close()
 
 def log_info(msg):
     global log_file
+    global current_state
     f = open(log_file, 'a')
     date = datetime.datetime.now().isoformat()
-    f.write("Info @ {}\n*******\n{}\n\n".format(date, msg))
+    f.write("Info @ {}\nState: {}\n*******\n{}\n\n".format(date, current_state, msg))
     f.close()
