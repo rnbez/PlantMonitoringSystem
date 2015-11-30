76ba6546d5a54f35f5f61079796124fdff945ae8
Rafael Nascimento Bezerra <rafaelbezerra.dev@gmail.com>
2015-11-29 23:57:08 -0500

    modifying the way the node shows the infos

--- monitoring_node/main.py
+++ monitoring_node/main.py
@@ -1,5 +1,6 @@
 from sensors import *
 from datetime import datetime, timedelta
+import screen as scn
 import sys, time, httpclient, node, api, json, log, actions, auth
 
 
@@ -10,26 +11,30 @@ def sendReadings(nextRun):
         air_temp = DHT11TemperatureSensor.read()
         log.current_state = "Posting Air Temperature"
         httpclient.post(api.__send_readings__, air_temp)
+        scn.update(env_temp=str(air_temp))
 
         log.current_state = "Reading Air Humidity"
         air_hum = DHT11HumiditySensor.read()
         log.current_state = "Posting Air Humidity"
         httpclient.post(api.__send_readings__, air_hum)
+        scn.update(env_hum=str(air_hum))
 
         log.current_state = "Reading Luminosity"
         air_lum = LDR.read()
         log.current_state = "Posting Luminosity"
         httpclient.post(api.__send_readings__, air_lum)
+        scn.update(env_lum=str(air_lum))
 
         log.current_state = "Reading Soil Temperature"
         soil_temp = DS18B20TemperatureSensor.read()
         log.current_state = "Reading Soil Temperature"
         httpclient.post(api.__send_readings__, soil_temp)
+        scn.update(soil_temp=str(soil_temp))
 
-        print air_lum["date"]
-        print "Env: Temp.: {0:0.2f}C  Humidity: {1:0.2f}%  &  Luminosity:{2:0.2f}%".format(air_temp['reading'], air_hum['reading'], air_lum['reading'])
-        print "Soil: Temp.: {0:0.2f}C  &  Moisture: 0%".format(soil_temp['reading'])
-        print "\n"
+        #print air_lum["date"]
+        #print "Env: Temp.: {0:0.2f}C  Humidity: {1:0.2f}%  &  Luminosity:{2:0.2f}%".format(air_temp['reading'], air_hum['reading'], air_lum['reading'])
+        #print "Soil: Temp.: {0:0.2f}C  &  Moisture: 0%".format(soil_temp['reading'])
+        #print "\n"
         log.log_info("System Ok")
 
         nextRun = datetime.now() + timedelta(seconds=60)
@@ -38,15 +43,23 @@ def sendReadings(nextRun):
 
 def updateNode(nextRun):
     if datetime.now() >= nextRun:
-	print 'Start updating the node...'
+        #print 'Start updating the node...'
         log.current_state = "Getting node info from server"
+        scn.update(node_updating="Updating node info [Calling Server]")
+
         response = httpclient.get(api.__get_node__(node.node_info['id']))
+        scn.update(server_resp="{} {}".format(response.status, response.reason))
+
         log.current_state = "Deserializing node info"
         body = json.loads(response.body)
         log.current_state = "Updating node info"
+        scn.update(node_updating="Updating node info [Saving Info]")
+
         node.update(body)
+        scn.update(node_updating="Last update at " + datetime.now().isoformat())
+
         nextRun = datetime.now() + timedelta(seconds=60)
-	print 'Node update finised\n'
+        #print 'Node update finised\n'
 
     return nextRun
 
@@ -54,6 +67,7 @@ def doActions():
     try:
         log.current_state = "Getting light and water info from server"
         response = httpclient.get(api.__get_lightwater__(node.node_info['id']))
+        scn.update(server_resp="{} {}".format(response.status, response.reason))
         log.current_state = "Deserializing light and water info from server"
         _node = json.loads(response.body)
         log.current_state = "Setting light pin"
@@ -61,10 +75,11 @@ def doActions():
         log.current_state = "Setting water pin"
         actions.setPin(actions.__water_pin__, _node['waterOn'])
     except TimeoutError:
-	error_msg = "HTTP Timeout - main.py @ doActions()"
-	print error_msg, "\n"
-	log.log_error(error_msg)
-	return
+    	error_msg = "HTTP Timeout - main.py @ doActions()"
+    	#print error_msg, "\n"
+        scn.update(last_error="Get Water and Light - at " + datetime.now().isoformat())
+    	log.log_error(error_msg)
+    	return
     return
 
 if __name__ == '__main__':
@@ -76,7 +91,7 @@ if __name__ == '__main__':
         log.current_state = "Authenticated"
         print "authentication succeeded"
     except:
-        error_msg = "authentication fail"
+        error_msg = "authentication failed"
         e = sys.exc_info()[0]
         print error_msg
         error_msg = "\n" + error_msg + "\n" + str(e)
@@ -110,18 +125,19 @@ if __name__ == '__main__':
     log.current_state = "GPIO pins set"
     print "\n----------------------\n"
 
+    scn.update()
+    scn.update(soil_moist = str(0)) # remove later
+
+
     nextSensorReading = datetime.now()
     nextNodeUpdate = datetime.now()
-    delay = 0.1
+    delay = 0.25
     while True:
         try:
             nextSensorReading = sendReadings(nextSensorReading)
-            doActions()
-	    time.sleep(delay)
-
             nextNodeUpdate = updateNode(nextNodeUpdate)
             doActions()
-	    time.sleep(delay)
+            time.sleep(delay)
         except KeyboardInterrupt:
             actions.clean()
             log.log_info("GPIO CleanUp")
@@ -133,4 +149,5 @@ if __name__ == '__main__':
             print error_msg
             error_msg = error_msg + str(e)
             log.log_error(error_msg)
+            scn.update(last_error="Unexpected Error - at " + datetime.now().isoformat())
             time.sleep(15)
